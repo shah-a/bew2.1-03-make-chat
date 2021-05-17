1bcf382d29a8484cb0c933b6a821b706282c0ba1
Ali Shah <s.ashah@outlook.com>
2021-05-17 14:12:56 -0400

    [Added] username functionality
    Notes:
    - Finished tutorial page 4

--- src/sockets/chat.js
+++ src/sockets/chat.js
@@ -1,3 +1,6 @@
 module.exports = (io, socket) => {
-  
+  socket.on('new user', (username) => {
+    console.log(`${username} has joined the chat! ✋`);
+    io.emit('new user', username);
+  })
 };
--- src/static/index.js
+++ src/static/index.js
@@ -1,4 +1,18 @@
-$(document).ready( () => {
-  //Connect to the socket.io server
+$(document).ready(() => {
   const socket = io.connect();
+
+  $('#create-user-btn').click((e) => {
+    e.preventDefault();
+    const username = $('#username-input').val();
+    if (username.length > 0) {
+      // Emit new `user event` to the server
+      socket.emit('new user', username);
+      $('.username-form').remove();
+    }
+  });
+
+  // Socket listeners
+  socket.on('new user', (username) => {
+    console.log(`${username} has joined the chat! ✋`);
+  })
 });
