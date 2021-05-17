31352561082af08521522da1fc301a487d73ebfe
Ali Shah <s.ashah@outlook.com>
2021-05-17 13:33:57 -0400

    [Added] socket boilerplate

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
+const http = require('http');
+const { join } = require('path');
 const express = require('express');
-const exphbs  = require('express-handlebars');
-const { Server } = require('http');
+const exphbs = require('express-handlebars');
+const { Server } = require('socket.io');
 
 const app = express();
-const server = Server(app);
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+app.use(express.static(join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
   res.render('index.handlebars');
+})
+
+io.on("connection", (socket) => {
+  console.log("🔌 New user connected! 🔌");
 })
 
 server.listen('3000', () => {
